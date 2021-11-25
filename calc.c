b17905975a50f3da9c41950e2d7a34336f4ece1b
andreajls <andrealopezsanz14@gmail.com>
2021-11-25 09:55:41 +0100

    calc.c

---
+++ calc.c
@@ -0,0 +1,15 @@
+int suma (int op1, int op2){
+    return (op1+op2);
+}
+
+int resta (int op1, int op2){
+    return (op1-op2);
+}
+
+int multiplica (int op1, int op2){
+    return (op1*op2);
+}
+
+int divideix (int op1, int op2){
+    return (op1/op2);
+}
